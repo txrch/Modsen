54aeea97f34beaf85a2df1589c2bce26e88e951b
txrch <90940845+txrch@users.noreply.github.com>
2025-02-15 16:36:16 +0300

    Task5.java

--- modsenpractice1/src/Task5.java
+++ modsenpractice1/src/Task5.java
@@ -3,17 +3,17 @@ public class Task5 {
         int left = 0, right = nums.length - 1;
 
         while (left <= right) {
-            int mid = left + (right - left) / 2; // Избегаем переполнения int
+            int mid = left + (right - left) / 2;
 
             if (nums[mid] == target) {
-                return mid; // Найден target, возвращаем индекс
+                return mid;
             } else if (nums[mid] < target) {
-                left = mid + 1; // Ищем в правой части
+                left = mid + 1;
             } else {
-                right = mid - 1; // Ищем в левой части
+                right = mid - 1;
             }
         }
 
-        return left; // Позиция, куда вставить target
+        return left;
     }
 }
